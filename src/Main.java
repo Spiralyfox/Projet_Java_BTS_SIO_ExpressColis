c4d547659f3db525b6f167ec9a255e580adc1211
Spiralyfox <dauriacmatteo@gmail.com@gmail.com>
2026-09-09 16:41:49 +0200

    Tests estEnFrance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args){
 
+        adresse a1 = new adresse("Meow","130606","Neptune","France");
+        adresse a2 = new adresse("Maow","606031","Saturne","Allemagne");
+
+        a1.estEnFrance();
+        a2.estEnFrance();
+
     }
 }
--- src/adresse.java
+++ src/adresse.java
@@ -1,12 +1,18 @@
 public class adresse {
+    private String adresseRue;
+    private String codePostal;
+    private String ville;
     private String pays;
 
-    public adresse(String pays) {
+    public adresse(String adresseRue, String codePostal, String ville, String pays) {
+        this.adresseRue = adresseRue;
+        this.codePostal = codePostal;
+        this.ville = ville;
         this.pays = pays;
     }
+
     public boolean estEnFrance() {
+        System.out.println(this.pays.equalsIgnoreCase("France"));
         return this.pays.equalsIgnoreCase("France");
     }
 }
-
-
